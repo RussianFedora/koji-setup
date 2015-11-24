ad0bbbc3637f86d35061ded836f1b1fdd07c8de3
Tigro <ashejn@gmail.com>
2015-11-24 20:48:20 +0300

    update url

--- gen_main_certs.sh
+++ gen_main_certs.sh
@@ -4,7 +4,7 @@ caname=koji
 openssl genrsa -out private/${caname}_ca_cert.key 2048
 openssl req -config ssl.cnf -new -x509 -days 3650 -key private/${caname}_ca_cert.key -out ${caname}_ca_cert.crt -extensions v3_ca
 
-for user in kojiweb kojihub koji.russianfedora.ru; do
+for user in kojiweb kojihub koji.russianfedora.pro; do
 	openssl genrsa -out certs/${user}.key 2048
 	openssl req -config ssl.cnf -new -nodes -out certs/${user}.csr -key certs/${user}.key
 	openssl ca -config ssl.cnf -keyfile private/${caname}_ca_cert.key -cert ${caname}_ca_cert.crt -out certs/${user}.crt -outdir certs -infiles certs/${user}.csr
